819013b8dac5b0252a8a19148141450d2cd77f47
EC2 Default User <ec2-user@ip-172-31-21-82.ap-southeast-1.compute.internal>
2018-12-13 20:53:13 +0000

    fixed carier_wave.rb

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,11 +4,11 @@ if Rails.env.production?
     config.fog_credentials = {
       # Amazon S3用の設定
       :provider              => 'AWS',
-      :region                => ENV['ap-northeast-1'],     # 例: 'ap-northeast-1'
-      :aws_access_key_id     => ENV['AKIAITBWBCX6PGY2HWUQ'],
-      :aws_secret_access_key => ENV['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']
+      :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['minitwi']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
 
