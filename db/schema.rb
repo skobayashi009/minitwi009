4553ab2ab375d56236908011b4e9dcca78ce33b7
EC2 Default User <ec2-user@ip-172-31-21-82.ap-southeast-1.compute.internal>
2018-12-13 21:52:18 +0000

    Finish user edit, update, index, and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181213170846) do
+ActiveRecord::Schema.define(version: 20181213214841) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20181213170846) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
