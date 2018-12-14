840d2919cbf69c844509ba2872f3d1036593f2ba
EC2 Default User <ec2-user@ip-172-31-21-82.ap-southeast-1.compute.internal>
2018-12-14 07:03:34 +0000

    Fixed navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require turbolinks
 //= require jquery
 //= require bootstrap
-//= require turbolinks
 //= require_tree .
-//cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js
-//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css
-//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js
