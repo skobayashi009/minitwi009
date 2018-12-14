43039b3e682e85ff3c691f01f058adf6841544dd
EC2 Default User <ec2-user@ip-172-31-21-82.ap-southeast-1.compute.internal>
2018-12-14 00:19:16 +0000

    Add user following

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,6 @@
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
+//cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js
+//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css
+//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js
